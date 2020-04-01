144cf0172c6e1fdb85e08eefecd0ab19708eada0
sangTran <sangthien2010@gmail.com>
2020-04-01 11:07:43 -0500

    adding Particles js package

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+import Particles from "react-particles-js";
 
+const particalOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles params={particalOptions} />
         <Navigation />
         <Logo />
         <Rank />
