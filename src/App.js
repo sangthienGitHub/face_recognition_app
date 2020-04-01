bf47f9ede5b0d7ea4fe985ff75dd6eac547e1607
sangTran <sangthien2010@gmail.com>
2020-04-01 11:14:58 -0500

    adjust Particles settings

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Particles from "react-particles-js";
 
 const particalOptions = {
   particles: {
-    line_linked: {
-      shadow: {
+    number: {
+      value: 100,
+      density: {
         enable: true,
-        color: "#3CA9D1",
-        blur: 5
+        value_area: 700
       }
     }
   }
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles params={particalOptions} />
+        <Particles className="particles" params={particalOptions} />
         <Navigation />
         <Logo />
         <Rank />
