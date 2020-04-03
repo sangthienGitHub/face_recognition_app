f55a188d7cea8c9fb0678c8af65f0163f8f80040
sangTran <sangthien2010@gmail.com>
2020-04-02 21:05:33 -0500

    set images to show in app through clarifai

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FaceRecognition = ({ imageUrl }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img
+          alt="face recognition container"
+          src={imageUrl}
+          width="500px"
+          heigh="auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
