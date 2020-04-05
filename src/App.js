acb150e757266a44e124bfd5425c5cf473951715
sangTran <sangthien2010@gmail.com>
2020-04-05 15:11:23 -0500

    connecting sigin app with backend on username and pass authentication

--- src/App.js
+++ src/App.js
@@ -37,6 +37,13 @@ class App extends Component {
     };
   }
 
+  //Test backend server
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/").then((response) =>
+  //     response.json().then(console.log)
+  //   );
+  // }
+
   calculateFaceLocation = (data) => {
     const clarifiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
