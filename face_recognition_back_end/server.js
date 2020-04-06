4d5bc5eba1fc0e137b0891bafd0e5fff86cdf8b6
sangTran <sangthien2010@gmail.com>
2020-04-06 14:32:05 -0500

    connecting postgreSQL with backend using Knex and pg packages

--- face_recognition_back_end/server.js
+++ face_recognition_back_end/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "test123",
+    database: "smartbrain",
+  },
+});
 
 const app = express();
 
@@ -45,13 +56,13 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
+  db("users")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then(console.log);
   res.json(database.users[database.users.length - 1]);
 });
 
