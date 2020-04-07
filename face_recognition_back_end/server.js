ececc8ecad16e78f23babba56811440f60fa487b
sangTran <sangthien2010@gmail.com>
2020-04-06 19:46:19 -0500

    adding port to server to deploy

--- face_recognition_back_end/server.js
+++ face_recognition_back_end/server.js
@@ -39,9 +39,8 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on part ${PORT}`);
-// });
+const PORT = process.env.PORT;
 
-// console.log(PORT);
+app.listen(PORT || 3000, () => {
+  console.log(`Server is listening on part ${PORT}`);
+});
